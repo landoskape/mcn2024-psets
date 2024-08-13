23b28742bef208c74226d97e531bf145c9fa829f
landoskape <andrewtylerlandau@gmail.com>
2024-08-12 23:11:24 -0400

    switching to rate models because I'm not crazy

--- intrinsic-excitability/models.py
+++ intrinsic-excitability/models.py
@@ -1,62 +1,61 @@
 import torch
 from torch import nn
-import snntorch as snn
-from snntorch import surrogate
-from layers import LeakyLayer, SynapticLayer
 
 
-class SpikingRNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, beta=1.0, synaptic=False, alpha=1.0):
+class GainRNN(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, input_rank=1, recurrent_rank=1):
         super().__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.output_size = output_size
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        self.input_rank = input_rank
+        self.recurrent_rank = recurrent_rank
 
         # Input layer
-        self.input_weight = torch.nn.Linear(input_size, hidden_size)
+        self.input_background = torch.randn((hidden_dim, input_dim)) / input_dim
+        self.input_projective = torch.nn.Parameter(torch.randn((hidden_dim, input_rank)))
+        self.input_receptive = torch.nn.Parameter(torch.randn((input_dim, input_rank)))
 
-        # Spiking recurrent layer
-        self.synaptic = synaptic
-        layer = SynapticLayer if synaptic else LeakyLayer
-        args = dict(alpha=alpha) if synaptic else {}
-        self.rnn = layer(self.hidden_size, beta=beta, spike_grad=surrogate.fast_sigmoid(), learn_beta=True, **args)
-        self.recurrent_weight = torch.nn.Linear(hidden_size, hidden_size, bias=False)
+        # Recurrent layer intrinsic properties
+        self.hidden_gain = torch.nn.Parameter(torch.randn(hidden_dim))
+        self.hidden_threshold = torch.nn.Parameter(torch.randn(hidden_dim))
+
+        # Recurrent layer connections
+        self.recurrent_background = torch.randn((hidden_dim, hidden_dim)) / hidden_dim
+        self.reccurent_projective = torch.nn.Parameter(torch.randn((hidden_dim, recurrent_rank)))
+        self.reccurent_receptive = torch.nn.Parameter(torch.randn((hidden_dim, recurrent_rank)))
 
         # Readout layer
-        self.readout = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.Softmax(dim=1)
+        self.readout = nn.Linear(hidden_dim, output_dim)
 
-    def forward(self, x, start_integration=0):
+    def activation(self, x):
+        return self.hidden_gain * torch.relu(x - self.hidden_threshold)
+
+    def input_weight(self):
+        return self.input_background + self.input_projective @ self.input_receptive.t()
+
+    def J(self):
+        return self.recurrent_background + self.reccurent_projective @ self.reccurent_receptive.t()
+
+    def forward(self, x, return_hidden=False):
         # Initialize hidden states and outputs
         batch_size = x.size(0)
-        mem = self.rnn.mem_reset(torch.zeros((batch_size, self.hidden_size)))
-        if self.synaptic:
-            syn = torch.zeros((batch_size, self.hidden_size))
-        spk = torch.zeros((batch_size, self.hidden_size))
-
-        spk_rec = []
-        mem_rec = []
-        if self.synaptic:
-            syn_rec = []
-        out = torch.zeros((batch_size, 2))
+        seq_length = x.size(1)
+        if return_hidden:
+            hidden = torch.zeros((batch_size, seq_length, self.hidden_dim), device=x.device)
+        out = torch.zeros((batch_size, seq_length, self.output_dim), device=x.device)
 
         # Recurrent loop
-        for step in range(x.size(1)):
-            x_in = self.input_weight(x[:, step])
-            r_in = self.recurrent_weight(spk)
-            if self.synaptic:
-                spk, syn, mem = self.rnn(x_in + r_in, syn=syn, mem=mem)
-            else:
-                spk, mem = self.rnn(x_in + r_in, mem=mem)
-            spk_rec.append(spk)
-            if self.synaptic:
-                syn_rec.append(syn)
-            mem_rec.append(mem)
-            if step >= start_integration:
-                c_out = self.readout(spk)
-                out += c_out
+        h = torch.zeros((batch_size, self.hidden_dim), device=x.device)
+        for step in range(seq_length):
+            x_in = (self.input_weight() @ x[:, step].T).T
+            r_in = (self.J() @ h.T).T
+            dh = -h + self.activation(r_in + x_in) / self.hidden_dim
+            h = h + dh
+            if return_hidden:
+                hidden[:, step] = h
+            out[:, step] = self.readout(h)
 
-        if self.synaptic:
-            return self.softmax(out), torch.stack(spk_rec, dim=1), torch.stack(syn_rec, dim=1), torch.stack(mem_rec, dim=1)
-        else:
-            return self.softmax(out), torch.stack(spk_rec, dim=1), torch.stack(mem_rec, dim=1)
+        if return_hidden:
+            return out, hidden
+        return out
--- intrinsic-excitability/tasks.py
+++ intrinsic-excitability/tasks.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 import torch
-from snntorch import spikegen
 
 
 class Task(ABC):
@@ -13,14 +12,16 @@ class Task(ABC):
         """Return the input dimensionality of the task"""
 
     @abstractmethod
-    def decision_start_time(self):
-        """Return the decision time of the task"""
+    def output_dimensionality(self):
+        """Return the output dimensionality of the task"""
 
 
-class GoNogo:
-    def __init__(self, D, T, sigma):
+class GoNogo(Task):
+    def __init__(self, D, sigma, stim_time=50, delay_time=10, decision_time=10):
         self.D = D
-        self.T = T
+        self.stim_time = stim_time
+        self.delay_time = delay_time
+        self.decision_time = decision_time
         self.sigma = sigma
 
         # Generate a random "cursor" vector
@@ -39,186 +40,203 @@ class GoNogo:
 
     def input_dimensionality(self):
         """return dimensionality of task input"""
-        return self.D
+        return self.D + 1
 
-    def decision_start_time(self):
-        """return time at which decision starts"""
-        return 0
+    def output_dimensionality(self):
+        return 2
 
-    def generate_data(self, B, sigma=None, source_strength=1.0, source_floor=0.0, data_norm=0.1):
+    def sequence_length(self):
+        return self.stim_time + self.delay_time + self.decision_time
+
+    def generate_data(self, B, sigma=None, delay_time=None, source_strength=1.0, source_floor=0.0):
         """Generate a batch of data with B batch elements"""
         # Generate random source strength
         source = self._generate_source(B, source_strength, source_floor)
 
         # Determine noise strength
         sigma = sigma or self.sigma
+        delay_time = delay_time or self.delay_time
 
         # Generate noise
-        noise = torch.randn(B, self.T, self.D) * sigma
+        noise = torch.randn(B, self.stim_time, self.D) * sigma
 
-        # Generate normalized data
+        # Add signal to noise
         X = noise + source.unsqueeze(1).unsqueeze(2) * self.cursor.unsqueeze(0).unsqueeze(1)
-        X = X / torch.norm(X, dim=2, keepdim=True) * data_norm
+        X = torch.cat([X, torch.zeros(B, delay_time + self.decision_time, self.D)], dim=1)
 
         # Measure the empirical source strength with noise, and generate labels
         s_empirical = torch.mean(X @ self.cursor, dim=1)
         labels = (s_empirical > 0).long()  # (targets - 1 means Go, 0 means NoGo)
 
-        # Data params
-        params = dict(
-            s_empirical=s_empirical,
-        )
-        return X, labels, params
+        # Add fixation trigger
+        fixation = torch.zeros((B, self.stim_time + delay_time + self.decision_time, 1))
+        fixation[:, : self.stim_time + delay_time] = 1.0
+        X = torch.cat([X, fixation], dim=2)
 
-
-class VarGoNogo(GoNogo):
-    """Go-Nogo with a variable threshold"""
-
-    def __init__(self, D, T, sigma):
-        self.D = D
-        self.T = T
-        self.sigma = sigma
-
-        # Generate a random "cursor" vector
-        self.cursor = self._generate_cursor()
-
-    def input_dimensionality(self):
-        """return dimensionality of task input"""
-        return self.D + 1
-
-    def generate_data(self, B, sigma=None, source_strength=1.0, source_floor=0.0, data_norm=0.1):
-        """Generate a batch of data with B batch elements"""
-        # Generate random source strength
-        source = self._generate_source(B, source_strength, source_floor)
-
-        # Generate random threshold and shift sources (so floor is consistent with threshold)
-        threshold = torch.randn(B)
-        source = source + threshold
-
-        # Determine noise strength
-        sigma = sigma or self.sigma
-
-        # Generate noise
-        noise = torch.randn(B, self.T, self.D) * sigma
-
-        # Generate normalized data
-        X = noise + source.unsqueeze(1).unsqueeze(2) * self.cursor.unsqueeze(0).unsqueeze(1)
-        X = X / torch.norm(X, dim=2, keepdim=True) * data_norm
-
-        # Measure the empirical source strength with noise, and generate labels
-        s_empirical = torch.mean(X @ self.cursor, dim=1)
-        labels = (s_empirical > threshold).long()  # (targets - 1 means Go, 0 means NoGo)
-
-        # Add threshold to X
-        X = torch.cat([X, threshold.unsqueeze(1).unsqueeze(2).expand(-1, self.T, 1)], dim=2)
+        # Build target
+        predecision = torch.zeros((B, self.stim_time + delay_time, self.output_dimensionality()))
+        postdecision = torch.zeros(B, self.output_dimensionality())
+        postdecision = torch.scatter(postdecision, 1, labels.unsqueeze(1), 1).unsqueeze(1).expand(-1, self.decision_time, -1)
+        target = torch.cat([predecision, postdecision], dim=1)
 
         # Data params
         params = dict(
+            sigma=sigma,
             s_empirical=s_empirical,
-            threshold=threshold,
         )
-        return X, labels, params
+        return X, target, params
 
 
-class ContextualGoNogo(GoNogo):
-    def __init__(self, D, sigma, num_contexts=2):
-        self.D = D
-        self.sigma = sigma
-        self.num_contexts = num_contexts
+# class VarGoNogo(GoNogo):
+#     """Go-Nogo with a variable threshold"""
 
-        assert self.num_contexts > 1, "Number of contexts must be greater than 1"
-        assert self.num_contexts < self.D, "Number of contexts must be less than the dimensionality"
+#     def __init__(self, D, T, sigma):
+#         self.D = D
+#         self.T = T
+#         self.sigma = sigma
 
-        # Generate a random "cursor" vector
-        self.cursors = torch.stack([self._generate_cursor() for _ in range(self.num_contexts)])
+#         # Generate a random "cursor" vector
+#         self.cursor = self._generate_cursor()
 
-        # Ensure that the cursor vectors are linearly independent
-        while not torch.linalg.matrix_rank(self.cursors) == self.num_contexts:
-            self.cursors = torch.stack([self._generate_cursor() for _ in range(self.num_contexts)])
+#     def input_dimensionality(self):
+#         """return dimensionality of task input"""
+#         return self.D + 1
 
-    def input_dimensionality(self):
-        """return dimensionality of task input"""
-        return self.D + self.num_contexts
+#     def output_dimensionality(self):
+#         return 2
 
-    def decision_start_time(self):
-        """return time at which decision starts"""
-        return 0
+#     def generate_data(self, B, sigma=None, source_strength=1.0, source_floor=0.0, data_norm=0.1):
+#         """Generate a batch of data with B batch elements"""
+#         # Generate random source strength
+#         source = self._generate_source(B, source_strength, source_floor)
 
-    def generate_data(self, B, T, sigma=None, source_strength=1.0, source_floor=0.0, data_norm=0.1):
-        """Generate a batch of data with B batch elements"""
-        # Generate random source strength
-        sources = torch.stack([self._generate_source(B, source_strength, source_floor) for _ in range(self.num_contexts)], dim=1)
-        cursor_signal = torch.sum(sources.unsqueeze(2) * self.cursors.unsqueeze(0), dim=1)
+#         # Generate random threshold and shift sources (so floor is consistent with threshold)
+#         threshold = torch.randn(B)
+#         source = source + threshold
 
-        # Determine noise strength
-        sigma = sigma or self.sigma
+#         # Determine noise strength
+#         sigma = sigma or self.sigma
 
-        # Generate noise
-        noise = torch.randn(B, T, self.D) * sigma
+#         # Generate noise
+#         noise = torch.randn(B, self.T, self.D) * sigma
 
-        # Generate normalized data
-        X = noise + cursor_signal.unsqueeze(1)
-        X = X / torch.norm(X, dim=2, keepdim=True) * data_norm
+#         # Generate normalized data
+#         X = noise + source.unsqueeze(1).unsqueeze(2) * self.cursor.unsqueeze(0).unsqueeze(1)
+#         X = X / torch.norm(X, dim=2, keepdim=True) * data_norm
 
-        # Choose a random context for each batch element
-        context_idx = torch.randint(self.num_contexts, (B,))
+#         # Measure the empirical source strength with noise, and generate labels
+#         s_empirical = torch.mean(X @ self.cursor, dim=1)
+#         labels = (s_empirical > threshold).long()  # (targets - 1 means Go, 0 means NoGo)
 
-        # Measure the empirical source strength with noise, and generate labels
-        s_empirical = torch.mean(X @ self.cursors.T, dim=1)
-        s_target = torch.gather(s_empirical, 1, context_idx.unsqueeze(1)).squeeze(1)
+#         # Add threshold to X
+#         X = torch.cat([X, threshold.unsqueeze(1).unsqueeze(2).expand(-1, self.T, 1)], dim=2)
 
-        labels = (s_target > 0).long()  # (targets - 1 means Go, 0 means NoGo)
-
-        # Add context index to X
-        context_inputs = torch.scatter(torch.zeros(B, self.num_contexts), 1, context_idx.unsqueeze(1), 1)
-
-        X = torch.cat([X, context_inputs.unsqueeze(1).expand(-1, T, -1)], dim=2)
-
-        # Data params
-        params = dict(
-            s_empirical=s_empirical,
-            context_idx=context_idx,
-        )
-        return X, labels, params
+#         # Data params
+#         params = dict(
+#             s_empirical=s_empirical,
+#             threshold=threshold,
+#         )
+#         return X, labels, params
 
 
-class DelayMatchSample:
-    def __init__(self, D, num_stimuli=2, stim_time=10, delay_time=10, decision_time=10, as_spikes=True, strength=0.5):
-        self.D = D
-        self.num_stimuli = num_stimuli
-        self.stim_time = stim_time
-        self.delay_time = delay_time
-        self.decision_time = decision_time
-        self.as_spikes = as_spikes
-        self.strength = strength
+# class ContextualGoNogo(GoNogo):
+#     def __init__(self, D, sigma, num_contexts=2):
+#         self.D = D
+#         self.sigma = sigma
+#         self.num_contexts = num_contexts
 
-    def input_dimensionality(self):
-        """return dimensionality of task input"""
-        return self.num_stimuli * self.D
+#         assert self.num_contexts > 1, "Number of contexts must be greater than 1"
+#         assert self.num_contexts < self.D, "Number of contexts must be less than the dimensionality"
 
-    def decision_start_time(self):
-        """return time at which decision starts"""
-        return 2 * self.stim_time + self.delay_time
+#         # Generate a random "cursor" vector
+#         self.cursors = torch.stack([self._generate_cursor() for _ in range(self.num_contexts)])
 
-    def generate_data(self, B):
-        """Generate a batch of data with B batch elements"""
-        # Generate random stimuli
-        stimuli = torch.randint(0, self.num_stimuli, (B, 2))
-        stim1 = torch.repeat_interleave(torch.scatter(torch.zeros(B, self.num_stimuli), 1, stimuli[:, 0].unsqueeze(1), 1), self.D, dim=1)
-        stim2 = torch.repeat_interleave(torch.scatter(torch.zeros(B, self.num_stimuli), 1, stimuli[:, 1].unsqueeze(1), 1), self.D, dim=1)
-        X = torch.cat(
-            [
-                stim1.unsqueeze(1).expand(-1, self.stim_time, -1),
-                torch.zeros((B, self.delay_time, self.D * self.num_stimuli)),
-                stim2.unsqueeze(1).expand(-1, self.stim_time, -1),
-                torch.zeros((B, self.decision_time, self.D * self.num_stimuli)),
-            ],
-            dim=1,
-        )
-        labels = (stimuli[:, 0] == stimuli[:, 1]).long()
-        params = dict(
-            stimuli=stimuli,
-        )
-        if self.as_spikes:
-            X = spikegen.rate(X, num_steps=1, gain=self.strength).squeeze(0)
-        return X, labels, params
+#         # Ensure that the cursor vectors are linearly independent
+#         while not torch.linalg.matrix_rank(self.cursors) == self.num_contexts:
+#             self.cursors = torch.stack([self._generate_cursor() for _ in range(self.num_contexts)])
+
+#     def input_dimensionality(self):
+#         """return dimensionality of task input"""
+#         return self.D + self.num_contexts
+
+#     def output_dimensionality(self):
+#         return 2
+
+#     def generate_data(self, B, T, sigma=None, source_strength=1.0, source_floor=0.0, data_norm=0.1):
+#         """Generate a batch of data with B batch elements"""
+#         # Generate random source strength
+#         sources = torch.stack([self._generate_source(B, source_strength, source_floor) for _ in range(self.num_contexts)], dim=1)
+#         cursor_signal = torch.sum(sources.unsqueeze(2) * self.cursors.unsqueeze(0), dim=1)
+
+#         # Determine noise strength
+#         sigma = sigma or self.sigma
+
+#         # Generate noise
+#         noise = torch.randn(B, T, self.D) * sigma
+
+#         # Generate normalized data
+#         X = noise + cursor_signal.unsqueeze(1)
+#         X = X / torch.norm(X, dim=2, keepdim=True) * data_norm
+
+#         # Choose a random context for each batch element
+#         context_idx = torch.randint(self.num_contexts, (B,))
+
+#         # Measure the empirical source strength with noise, and generate labels
+#         s_empirical = torch.mean(X @ self.cursors.T, dim=1)
+#         s_target = torch.gather(s_empirical, 1, context_idx.unsqueeze(1)).squeeze(1)
+
+#         labels = (s_target > 0).long()  # (targets - 1 means Go, 0 means NoGo)
+
+#         # Add context index to X
+#         context_inputs = torch.scatter(torch.zeros(B, self.num_contexts), 1, context_idx.unsqueeze(1), 1)
+
+#         X = torch.cat([X, context_inputs.unsqueeze(1).expand(-1, T, -1)], dim=2)
+
+#         # Data params
+#         params = dict(
+#             s_empirical=s_empirical,
+#             context_idx=context_idx,
+#         )
+#         return X, labels, params
+
+
+# class DelayMatchSample(Task):
+#     def __init__(self, D, num_stimuli=2, stim_time=10, delay_time=10, decision_time=10, as_spikes=True, strength=0.5):
+#         self.D = D
+#         self.num_stimuli = num_stimuli
+#         self.stim_time = stim_time
+#         self.delay_time = delay_time
+#         self.decision_time = decision_time
+#         self.as_spikes = as_spikes
+#         self.strength = strength
+
+#     def input_dimensionality(self):
+#         """return dimensionality of task input"""
+#         return self.num_stimuli * self.D
+
+#     def decision_start_time(self):
+#         """return time at which decision starts"""
+#         return 2 * self.stim_time + self.delay_time
+
+#     def generate_data(self, B):
+#         """Generate a batch of data with B batch elements"""
+#         # Generate random stimuli
+#         stimuli = torch.randint(0, self.num_stimuli, (B, 2))
+#         stim1 = torch.repeat_interleave(torch.scatter(torch.zeros(B, self.num_stimuli), 1, stimuli[:, 0].unsqueeze(1), 1), self.D, dim=1)
+#         stim2 = torch.repeat_interleave(torch.scatter(torch.zeros(B, self.num_stimuli), 1, stimuli[:, 1].unsqueeze(1), 1), self.D, dim=1)
+#         X = torch.cat(
+#             [
+#                 stim1.unsqueeze(1).expand(-1, self.stim_time, -1),
+#                 torch.zeros((B, self.delay_time, self.D * self.num_stimuli)),
+#                 stim2.unsqueeze(1).expand(-1, self.stim_time, -1),
+#                 torch.zeros((B, self.decision_time, self.D * self.num_stimuli)),
+#             ],
+#             dim=1,
+#         )
+#         labels = (stimuli[:, 0] == stimuli[:, 1]).long()
+#         params = dict(
+#             stimuli=stimuli,
+#         )
+#         if self.as_spikes:
+#             X = spikegen.rate(X, num_steps=1, gain=self.strength).squeeze(0)
+#         return X, labels, params
